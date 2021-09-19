3434a3eb9f24c988ca00068f0d5b3089e1540b0a
Anil Paswan <33987849+anilpaswan619@users.noreply.github.com>
2021-09-19 21:48:57 +0530

    Create index.js

---
+++ Basics/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import City from './city';
+import './style.css';
+
+ReactDOM.render(<City/>, document.getElementById('root'));
